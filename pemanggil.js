e80961f3e8218cada7c6160bcbd88b25ffeb6a7b
suaranime <31305649+suaranime@users.noreply.github.com>
2017-08-25 00:27:29 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -24,7 +24,7 @@ var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/08/keuntungan-memiliki-website-download.html"}
 if(x=="2"){xxx="/2017/08/trading-bitcoin-di-vip-bitcoin-dapatkan.html"}
-if(x=="3"){xxx="/2017/08/cara-mendapatkan-uang-di-uc-news.html"}a_to_ve[i].href="http://www.suaranimesf.ga/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/2017/08/cara-mendapatkan-uang-di-uc-news.html"}a_to_ve[i].href="http://www.suaranimesf.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
